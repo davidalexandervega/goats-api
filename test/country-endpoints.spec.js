58b6128b7bf57e06d5f89dead7997a4b5825c074
killeraliens <killeraliens@outlook.com>
2019-12-18 14:22:58 -0700

    event schema

--- test/country-endpoints.spec.js
+++ test/country-endpoints.spec.js
@@ -2,5 +2,38 @@ const app = require('../src/app/app')
 const knex = require('knex')
 
 describe('Country endpoints', () => {
+  let db;
+  before('create knex db instance', () => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DB_URL
+    })
+    app.set('db', db)
+  })
 
+  before('clears country table', () => {
+    return db.raw('TRUNCATE city, country, app_user')
+  })
+
+  beforeEach('clears country table', () => {
+    return db.raw('TRUNCATE city, country, app_user')
+  })
+
+  afterEach('clears country table', () => {
+    return db.raw('TRUNCATE city, country,  app_user')
+  })
+
+  after('kill knex db', () => {
+    return db.destroy()
+  })
+
+  describe('GET /api/country endpoint', () => {
+    context('given there are no countries in db', () => {
+      it('responds with 200 and empty array', () => {
+        return supertest(app)
+          .get('/api/country')
+          .expect(200, [])
+      })
+    })
+  })
 })
--- test/event-endpoints.spec.js
+++ test/event-endpoints.spec.js
@@ -2,6 +2,31 @@ const app = require('../src/app/app')
 const knex = require('knex')
 
 describe('Event endpoints', () => {
+  let db;
+  before('create knex db instance', () => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DB_URL
+    })
+    app.set('db', db)
+  })
+
+  before('clears event table', () => {
+    return db.truncate('event')
+  })
+
+  beforeEach('clears event table', () => {
+    return db.truncate('event')
+  })
+
+  afterEach('clears event table', () => {
+    return db.truncate('event')
+  })
+
+  after('kill knex db', () => {
+    return db.destroy()
+  })
+
   describe('GET /api/event endpoint', () => {
     context('given there are no events in db', () => {
       it('responds with 200 and empty array', () => {
