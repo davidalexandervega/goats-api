a2c6be47bd5989160d1bf00d5a6ba121f3029fc3
killeraliens <killeraliens@outlook.com>
2020-02-07 16:46:13 -0700

    country endpoint tests

--- test/country-endpoints.spec.js
+++ test/country-endpoints.spec.js
@@ -2,7 +2,7 @@ const app = require('../src/app')
 const knex = require('knex')
 const { seed, truncate } = require('./seed-fixtures')
 
-describe('Country endpoints', () => {
+describe.only('Country endpoints', () => {
   let db;
   before('create knex db instance', () => {
     db = knex({
@@ -36,5 +36,28 @@ describe('Country endpoints', () => {
           .expect(200, [])
       })
     })
+
+    context('there are countries loaded', () => {
+      beforeEach('seed countries', () => {
+        return db.raw(seed.countryRegionCity())
+      })
+      it('responds with 200 and array of all countries', () => {
+        return supertest(app)
+          .get('/api/country')
+          .expect(200)
+          .expect(res => {
+            expect(res.body.length).to.eql(251)
+            expect(res.body[0]).to.have.property('country_code')
+            expect(res.body[0].country_code.length).to.eql(2)
+            expect(res.body[0]).to.have.property('country_name')
+            expect(res.body[0].country_name.length).to.be.at.least(1)
+            expect(res.body[0]).to.have.property('lat')
+            expect(res.body[0]).to.have.property('lng')
+            expect(res.body[0]).to.have.property('image_url')
+          })
+      })
+    })
   })
+
+
 })
