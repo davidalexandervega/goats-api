fbf8678414c182c6c377934fec49abd9f8529923
killeraliens <killeraliens@outlook.com>
2020-04-01 15:42:18 -0700

    sendgrid config

--- src/config/auth-config.js
+++ src/config/auth-config.js
@@ -4,6 +4,8 @@ module.exports = {
     clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     callbackURL: process.env.FACEBOOK_CALLBACK_URL || `http:localhost://${process.env.PORT}/auth/facebook/callback`,
     profileUrl: 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email'
+  },
+  sendgridAuth: {
+    apiKey: process.env.SENDGRID_API_KEY
   }
-
 }
