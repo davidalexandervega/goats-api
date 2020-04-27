f66a7e6f278e5966375a22f3d02975fa219fbfab
killeraliens <killeraliens@outlook.com>
2020-04-27 09:36:56 -0700

    new email troublshoot

--- src/config/config.js
+++ src/config/config.js
@@ -5,5 +5,6 @@ module.exports = {
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || `psql://goats_api:${process.env.DATABASE_PW}@localhost/goats_api_test`,
   CLIENT_ENDPOINT: process.env.NODE_ENV === "production"
     ? `https://unholygrail.org/`
+    // ? `https://goatsguide.com/`
     : `https://localhost:3000/`
 }
