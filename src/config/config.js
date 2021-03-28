4d6956662e90809f88679a4f34d3409b11e232a9
killeraliens <killeraliens@outlook.com>
2021-03-28 14:14:08 -0700

    ssl true test

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DATABASE_URL: process.env.DATABASE_URL || `psql://goats_api:${process.env.DATABASE_PW}@localhost/goats_api`,
+  DATABASE_URL: process.env.DATABASE_URL + `?ssl=true` || `psql://goats_api:${process.env.DATABASE_PW}@localhost/goats_api`,
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || `psql://goats_api:${process.env.DATABASE_PW}@localhost/goats_api_test`,
   CLIENT_ENDPOINT: process.env.NODE_ENV === "production"
     ? `https://unholygrail.org/`
