c4c35b5a92f49bb94b5133eaf0d64622c578c316
acampbell <acampbell@cleardemand.com>
2022-03-11 16:49:35 -0700

    pg ssl deprecated

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL: process.env.NODE_ENV === "production"
-    ? process.env.DATABASE_URL + `?ssl=true`
+    ? process.env.DATABASE_URL
     : process.env.DATABASE_URL,
   SEEDS_PATH: process.env.SEEDS_PATH || '/Users/user/code/killeraliens/goats-api/seeds/',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || `psql://goats_api:${process.env.DATABASE_PW}@localhost/goats_api_test`,
