b620dfde056aee834abafdfef3842534304899ab
acampbell <acampbell@cleardemand.com>
2022-03-12 09:43:56 -0700

    heroku depoy 503 rejectUnauthorized trial

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL: process.env.NODE_ENV === "production"
-    ? process.env.DATABASE_URL + `?ssl=true`
+    ? process.env.DATABASE_URL //+ `?ssl=true`
     : process.env.DATABASE_URL,
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || `psql://${process.env.DATABASE_USER}:${process.env.DATABASE_PW}@${process.env.DATABASE_HOST}/${process.env.TEST_DATABASE_NAME}`,
   DATABASE_CONNECT: {
