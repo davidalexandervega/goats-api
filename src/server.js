9aa2fbc40017e569d3a73125148a2de7cd833685
acampbell <acampbell@cleardemand.com>
2022-03-12 09:47:58 -0700

    heroku depoy 503 rejectUnauthorized trial

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const knex = require('knex')
 const db = knex({
   client: 'pg',
   connection: {
-    connectionString: process.env.DATABASE_URL,
+    connectionString: DATABASE_URL,
     ssl: { rejectUnauthorized }//process.env.NODE_ENV === "production"
   }
 })
